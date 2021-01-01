2785e57e55d35d5f72329b43b55a553ad412c662
AlexBosp <bobrovalexeye@gmail.com>
2021-01-01 22:16:33 +0300

    added csl ports; #CSL-23, #CSL-4

---
+++ ports/csl/portfile.cmake
@@ -0,0 +1,26 @@
+
+set(PortName csl)
+
+vcpkg_from_git(
+  OUT_SOURCE_PATH SOURCE_PATH
+  URL https://bitbucket.org/AlexeyBobrov/${PortName}
+  REF 20a104ae171a1a661af5a951e7497677872e0828
+  )
+
+vcpkg_configure_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS
+	-DBUILD_WITHOUT_TESTS=ON
+  )
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PortName})
+
+set(csl_DIR lib/cmake/${PortName})
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+# Handle copyright
+configure_file(${SOURCE_PATH}/License.txt ${CURRENT_PACKAGES_DIR}/share/${PortName}/copyright COPYONLY)
+
+vcpkg_copy_pdbs()
