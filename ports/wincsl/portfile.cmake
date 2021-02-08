f07a5e66b1181809de1f6909e34389d2a7b5349e
Alexey Bobrov <bobrovalexeye@gmail.com>
2021-02-08 20:05:23 +0300

    add wincsl projects, change commit csl project; #CSL-31

--- ports/wincsl/portfile.cmake
+++ ports/wincsl/portfile.cmake
@@ -1,8 +1,8 @@
 
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH
-  URL https://bitbucket.org/AlexeyBobrov/csl
-  REF 75258f919df432b6e152b2cb156d0eeac2451323
+  URL https://bitbucket.org/AlexeyBobrov/wincsl
+  REF 7991bc334ee2d55ae4da8df8d0da6a706d6723da
   )
 
 vcpkg_configure_cmake(
@@ -13,13 +13,13 @@ vcpkg_configure_cmake(
 	-DWITH_UNICODE=ON
   )
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/csl)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/wincsl)
 
-set(csl_DIR lib/cmake/csl)
+set(csl_DIR lib/cmake/wincsl)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
 # Handle copyright
-configure_file(${SOURCE_PATH}/License.txt ${CURRENT_PACKAGES_DIR}/share/csl/copyright COPYONLY)
+configure_file(${SOURCE_PATH}/License.txt ${CURRENT_PACKAGES_DIR}/share/wincsl/copyright COPYONLY)
 
 vcpkg_copy_pdbs()
